b5592ef1d255c820ed66bd7ff12291dffd486589
Greizgh <greizgh@ephax.org>
2015-06-07 17:50:13 +0200

    Enlarge your lolcat

---
+++ README.md
@@ -0,0 +1,29 @@
+#Materialbox
+
+This [pelican](http://blog.getpelican.com/) plugin adds necessary tags to use _materialboxed_ images with [materialize](http://materializecss.com/) css framework.
+
+See the demo from [materialize doc](http://materializecss.com/media.html#materialbox)
+
+##Requirements
+
+This plugin needs [beautifulsoup](http://www.crummy.com/software/BeautifulSoup/).
+You can install it with pip:
+
+    pip install beautifulsoup4
+
+##How to use
+
+Simply add `materialbox` to your pelican plugins.
+
+An image included like this:
+```rst
+.. figure:: https://127.0.0.1/my_awesome.gif
+    :alt: cats
+
+    This is image caption
+```
+
+Will result in this:
+```html
+<img alt="cats" class="materialboxed" data-caption="This is image caption" src="https://127.0.0.1/my_awesome.gif">
+```
---
+++ materialbox.py
@@ -0,0 +1,26 @@
+"""
+Materialbox
+-----------
+
+This plugin adds necessary attributes to img tags to work with
+the materialboxed class from materialize css framework.
+"""
+from pelican import signals
+from bs4 import BeautifulSoup
+
+def materialbox(instance):
+    if instance._content is not None:
+        content = instance._content
+        soup = BeautifulSoup(content)
+        for figure in soup.find_all("div", class_="figure"):
+            caption = figure.find('p', class_="caption").string
+            img = figure.find('img')
+            img['data-caption'] = caption
+            if 'class' not in img.attrs:
+                img['class'] = "materialboxed"
+            else:
+                img['class'].append("materialboxed")
+        instance._content = soup.decode()
+
+def register():
+    signals.content_object_init.connect(materialbox)
